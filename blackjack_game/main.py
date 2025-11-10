446af1c30ed730a597f0f8873744a1f96891b5cb
Parthasarathi Mitra <parthasmcomp@gmail.com>
2025-11-10 23:10:41 +0530

    added helper functions for blackjack text game

---
+++ blackjack_game/main.py
@@ -0,0 +1,88 @@
+import random
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
+    
+class Deck:
+    def __init__(self):
+        suits = ["spades", "clubs", "diamonds", "hearts"]
+        ranks = [{"rank": "A", "value": 11}, 
+                {"rank": "2", "value": 2}, 
+                {"rank": "3", "value": 3}, 
+                {"rank": "4", "value": 4}, 
+                {"rank": "5", "value": 5}, 
+                {"rank": "6", "value": 6}, 
+                {"rank": "7", "value": 7}, 
+                {"rank": "8", "value": 8}, 
+                {"rank": "9", "value": 9}, 
+                {"rank": "10", "value": 10}, 
+                {"rank": "J", "value": 10}, 
+                {"rank": "Q", "value": 10}, 
+                {"rank": "K", "value": 10}]
+        self.cards = []
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+
+    def shuffle_deck(self):
+        if len(self.cards) > 1:
+            random.shuffle(self.cards)    
+
+    def deal_card(self, number):
+        cards_dealt = []
+        if len(self.cards) > 0:
+            for x in range(number):
+                cards_dealt.append(self.cards.pop())
+        return cards_dealt
+    
+class Hand:
+    def __init__(self, dealer=False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
+
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            card_value = int(card.rank["value"])
+            self.value += card_value
+            if card.rank["rank"] == "A":
+                has_ace = True
+        if has_ace and self.value > 21:
+            self.value -= 10
+
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+    
+    def is_blackjack(self):
+        return self.get_value() == 21
+    
+    def display_hand(self, show_all_dealer_cards = False):
+        print(f'''{"Dealer's" if self.dealer else "Your"} hand:''')
+        for index, card in enumerate(self.cards):
+            if index == 0 \
+                and self.dealer \
+                and not show_all_dealer_cards \
+                and not self.is_blackjack():
+                print("Hidden")
+            else:
+                print(card)
+        if not self.dealer:
+            print(f"Value: {self.get_value()}")
+        print()
+
+deck1 = Deck()
+deck1.shuffle_deck()
+
+hand = Hand()
+hand.add_card(deck1.deal_card(2))
+hand.display_hand()
